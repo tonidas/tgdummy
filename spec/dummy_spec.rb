ac6ac0fa8ade329d80c5fae89e4341abb42ea8ec
Airton Sobral <airton@ubee.in>
2016-12-21 13:09:33 -0300

    Merge branch 'release/0.3.0'

--- spec/dummy_spec.rb
+++ spec/dummy_spec.rb
@@ -2,8 +2,8 @@ require File.join(File.dirname(__FILE__), "..", "dummy")
 
 describe Dummy do
   describe "#my_name" do
-    it "should return \"dummy\"" do
-      expect(described_class.new.my_name).to eq("Dummy")
+    it "should return \"my name is Dummy\"" do
+      expect(described_class.new.my_name).to eq("my name is Dummy")
     end
   end
 end
